fd8b7eec510b57e487be43f366a09ef74df38a79
ikaroamorim <ikaro.amorim@gmail.com>
2021-10-23 19:12:27 -0300

    Conclusion of Chapter 02 Selectors

--- jqueryFundamentals/02Selectors/scripts/script.js
+++ jqueryFundamentals/02Selectors/scripts/script.js
@@ -20,7 +20,36 @@ jQuery(document).ready(function () {
    //Selecionando por classe
    $('a.hide').hide();
 
-   //Selecionanod por atributo
+   //Selecionano por atributo
    //$('a[title]')
    $('a[title="Teste"]').css('border', '2px solid black')
+
+   $('input[type="text"')
+
+
+   //Seleciona todos os elementos de entrada : input, select, textarea, button, image, radio...
+   $(':input')
+
+   var inputs = $(':input');
+   //Não adianta pegar tentar usar funções jQuery de elementos html, tem que envolvero num jquery
+   // inputs[0].val() --> Erro
+   console.log( $(inputs[0]).val() )
+
+   //Podemos iterar entre os inputs, e o comportamento será um pouco diferente
+   $(':input').each(function(){
+      var elem = $(this); //Nesse caso já está envolvido no jQuery
+      console.log(elem.val())
+   })
+
+   console.log('Exemplo de contains',$('p:contains("Exemplo")'));
+
+   $('p:odd').css('background-color','gray')
+   $('p:even').css('background-color','gold')
+
+   $('div[title $= "Title"]').html();  // Termina com Title 
+   //   $= termina com
+   //   ^= começa com 
+   //   *= contém
+
+
 });
