0138aaa6c5642a1d9dc6e3dc62cf7206784a62ca
ikaroamorim <ikaro.amorim@gmail.com>
2021-10-24 10:22:55 -0300

    Starting Chapter 03 Interacting with DOM

--- jqueryFundamentals/03InteractingDOM/scripts/script.js
+++ jqueryFundamentals/03InteractingDOM/scripts/script.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function () {
 
+$('p').each( function(index, elem){
+   console.log(index + ' = '+ $(elem).text());
+
+   //console.log(index + ' = '+ $(this).text()); pode ser usado o this e não seria necessário
+   //passá-lo como parametro da função
+
+   //Cuidado com o que vai dentro do each, é um loop e o que tiver dentro será executado várias vezes
+})
+
 
 });
