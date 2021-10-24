244145dea666eefe8b4a6e4a5601d51f3f383cdf
ikaroamorim <ikaro.amorim@gmail.com>
2021-10-24 12:19:55 -0300

    Completed Chapter 03

--- jqueryFundamentals/03InteractingDOM/scripts/script.js
+++ jqueryFundamentals/03InteractingDOM/scripts/script.js
@@ -10,4 +10,42 @@ $('p').each( function(index, elem){
 })
 
 
+$('a').attr('href','http://www.youtube.com')
+
+//Dá para usar JSON para mudar os atributos
+$('a').attr({
+   title: 'My Anchor Title',
+   style: 'color: red'
+});
+
+// Chaining
+$('p').attr('title', 'Paragraph Title').css('color','#555')
+
+var mc = $('#myContent')
+
+//Append
+mc.append('<div class="alert alert-primary" role="alert">Alerta appendada</div>');
+//$('<div class="alert alert-primary" role="alert">A simple primary alert—check it out!</div>').appendTo('h1')
+
+//Prepend
+mc.prepend('<div class="alert alert-secondary" role="alert">Alerta prependado</div>');
+
+//Wrap
+$('#myContent, form').wrap('<div class="container"><div class="row"><div class="col-6"></div></div></div>');
+
+$('p:last-child').remove()
+
+mc.css({
+   'background-color':"#eef",
+   'font-size':'16px'
+})
+
+/*
+addClass
+hasClass
+removeClass
+toggleClass
+*/
+$('nav').addClass('navbar-dark bg-dark').removeClass('navbar-light bg-light')
+
 });
